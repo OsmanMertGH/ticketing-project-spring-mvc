8fe2599c7aed5d864ad4ef4486923ceb756065af
Osman MERT <osman_mrt@yahoo.com>
2023-08-21 15:48:28 +0300

    Roles added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model) {
 
         model.addAttribute("user",new UserDTO());
 
-        model.addAttribute("roles",);
+        model.addAttribute("roles", roleService.findAll());
 
         return "/user/create";
     }
