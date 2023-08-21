a02810f2dea0d1306bce5f0e91f0f97ba7459e13
Osman MERT <osman_mrt@yahoo.com>
2023-08-21 11:22:26 +0300

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectSpringMvcApplication.class, args);
+    }
+
+}
